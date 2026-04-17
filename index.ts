59d5841986e7180776527285c1c9f53e2f903707
Phạm Quang Trường <truongpham090325@gmail.com>
2026-04-17 15:20:45 +0700

    Khởi tạo dự án

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import adminRoutes from "./routes/admin/index.route";
+import clientRoutes from "./routes/client/index.route";
+const app = express();
+const port = 4000;
+
+// Thiết lập thư mục views và view engine Pug
+app.set("views", path.join(__dirname, "views")); // Thư mục chứa file Pug
+app.set("view engine", "pug"); // Thiết lập Pug làm view engine
+
+// Thiết lập thư mục chứa file tĩnh
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use("/", clientRoutes);
+
+app.listen(port, () => {
+  console.log(`Website đang chạy trên cổng ${port}`);
+});
---
+++ routes/admin/index.route.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import dashboardRoutes from "./dashboard.route";
+
+const router = Router();
+
+router.use('/dashboard', dashboardRoutes);
+
+export default router;
