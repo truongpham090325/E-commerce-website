c417d0f58330e35a8110fbb016a7ab8721607383
Phạm Quang Trường <truongpham090325@gmail.com>
2026-04-22 14:27:19 +0700

    Sửa Startbar bên admin

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import express from "express";
 import path from "path";
 import adminRoutes from "./routes/admin/index.route";
 import clientRoutes from "./routes/client/index.route";
+import { pathAdmin } from "./configs/variable.config";
 const app = express();
-const port = 4000;
+const port = 3000;
 
 // Thiết lập thư mục views và view engine Pug
 app.set("views", path.join(__dirname, "views")); // Thư mục chứa file Pug
@@ -12,7 +13,10 @@ app.set("view engine", "pug"); // Thiết lập Pug làm view engine
 // Thiết lập thư mục chứa file tĩnh
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+// Tạo biến toàn cục trong file Pug
+app.locals.pathAdmin = pathAdmin;
+
+app.use(`/${pathAdmin}`, adminRoutes);
 app.use("/", clientRoutes);
 
 app.listen(port, () => {
