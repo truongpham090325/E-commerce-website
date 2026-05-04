0bf7cb8c009ad17f09e9971a68bc7c828ac4d886
Phạm Quang Trường <truongpham090325@gmail.com>
2026-05-04 12:00:29 +0700

    Làm trang thùng rác lịch sử, xóa mềm, khôi phục, xóa vĩnh viễn

--- routes/admin/admin-log.route.ts
+++ routes/admin/admin-log.route.ts
@@ -5,4 +5,12 @@ const router = Router();
 
 router.get("/list", adminLogController.list);
 
+router.patch("/delete/:id", adminLogController.deletePatch);
+
+router.get("/trash", adminLogController.trash);
+
+router.patch("/undo/:id", adminLogController.undoPatch);
+
+router.delete("/destroy/:id", adminLogController.destroyDelete);
+
 export default router;
