9e4008f3b8c140a0ad42f9dc551942069880e4f9
Phạm Quang Trường <truongpham090325@gmail.com>
2026-05-08 09:43:10 +0700

    Làm trang tạo mã giảm giá + tính năng Tạo Mã giảm giá

---
+++ routes/admin/coupon.route.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import * as couponController from "../../controllers/admin/coupon.controller";
+import * as couponValidate from "../../validations/admin/coupon.validate";
+import multer from "multer";
+
+const router = Router();
+
+const upload = multer();
+
+router.get("/create", couponController.create);
+
+router.post(
+  "/create",
+  upload.none(),
+  couponValidate.createPost,
+  couponController.createPost,
+);
+
+export default router;
