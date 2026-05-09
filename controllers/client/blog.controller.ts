8bc4b98fb1c97f925b088b8ea184dc4d7ca50e17
Phạm Quang Trường <truongpham090325@gmail.com>
2026-05-09 09:20:37 +0700

    Hiển thị dữ liệu ra PAGE BANNER

--- controllers/client/blog.controller.ts
+++ controllers/client/blog.controller.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from "express";
+import CategoryBlog from "../../models/category-blog.model";
 
-export const blogByCategory = (req: Request, res: Response) => {
-  console.log(req.params.slug);
+export const blogByCategory = async (req: Request, res: Response) => {
+  try {
+    const categoryDetail = await CategoryBlog.findOne({
+      slug: req.params.slug,
+      deleted: false,
+      status: "active",
+    });
 
-  res.render("client/pages/blog-by-category", {
-    pageTitle: "Danh sách bài viết theo danh mục",
-  });
+    if (!categoryDetail) {
+      res.redirect("/");
+      return;
+    }
+
+    res.render("client/pages/blog-by-category", {
+      pageTitle: "Danh sách bài viết theo danh mục",
+      categoryDetail: categoryDetail,
+    });
+  } catch (error) {
+    res.redirect("/");
+  }
 };
