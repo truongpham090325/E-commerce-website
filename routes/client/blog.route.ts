e3e19be1e9412d5d510fcaeac92cfb65f1a723a9
Phạm Quang Trường <truongpham090325@gmail.com>
2026-05-09 11:19:05 +0700

    Hiển thị dữ liệu ra Popular Blog

--- routes/client/blog.route.ts
+++ routes/client/blog.route.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import * as blogController from "../../controllers/client/blog.controller";
+import * as blogMiddleware from "../../middlewares/client/blog.middleware";
 
 const router = Router();
 
-router.get("/category/:slug", blogController.blogByCategory);
+router.get(
+  "/category/:slug",
+  blogMiddleware.getPopularBlog,
+  blogController.blogByCategory,
+);
 
 export default router;
