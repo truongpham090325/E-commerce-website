7aaffba4d7c4936a6c598b5dc8bf8ff559e82111
Phạm Quang Trường <truongpham090325@gmail.com>
2026-05-10 11:26:26 +0700

    Tạo giao diện trang danh sách sản phẩm

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import homeRoutes from "./home.route";
 import blogRoutes from "./blog.route";
+import productRoutes from "./product.route";
+
 import * as categoryMiddleware from "../../middlewares/client/category.middleware";
 
 const router = Router();
@@ -9,5 +11,6 @@ router.use(categoryMiddleware.getAllCategory);
 
 router.use("/", homeRoutes);
 router.use("/blog", blogRoutes);
+router.use("/product", productRoutes);
 
 export default router;
