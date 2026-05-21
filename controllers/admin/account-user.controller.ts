44b3fc8c102104c9826f16ab2458748b85f263d7
Phạm Quang Trường <truongpham090325@gmail.com>
2026-05-21 15:37:03 +0700

    Làm trang thùng rác tài khoản người dùng, tính năng xóa, khôi phục tài khoản

--- controllers/admin/account-user.controller.ts
+++ controllers/admin/account-user.controller.ts
@@ -49,3 +49,132 @@ export const list = async (req: Request, res: Response) => {
     pagination: pagination,
   });
 };
+
+export const deletePatch = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const accountDetail = await AccountUser.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    if (!accountDetail) {
+      res.json({
+        code: "error",
+        message: "Bản ghi không tồn tại!",
+      });
+      return;
+    }
+
+    await AccountUser.updateOne(
+      {
+        _id: id,
+      },
+      {
+        deleted: true,
+        deletedAt: Date.now(),
+      },
+    );
+
+    res.json({
+      code: "success",
+      message: "Xóa tài khoản thành công!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Bản ghi không hợp lệ!",
+    });
+  }
+};
+
+export const trash = async (req: Request, res: Response) => {
+  const find: {
+    deleted: boolean;
+    search?: RegExp;
+  } = {
+    deleted: true,
+  };
+
+  const accountUserList: any = await AccountUser.find(find);
+
+  res.render("admin/pages/account-user-trash", {
+    pageTitle: "Thùng rác tài khoản người dùng",
+    accountUserList: accountUserList,
+  });
+};
+
+export const undoPatch = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const accountDetail = await AccountUser.findOne({
+      _id: id,
+      deleted: true,
+    });
+
+    if (!accountDetail) {
+      res.json({
+        code: "error",
+        message: "Bản ghi không tồn tại!",
+      });
+      return;
+    }
+
+    await AccountUser.updateOne(
+      {
+        _id: id,
+      },
+      {
+        deleted: false,
+      },
+    );
+
+    res.json({
+      code: "success",
+      message: "Khôi phục tài khoản thành công!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Bản ghi không hợp lệ!",
+    });
+  }
+};
+
+export const destroyDelete = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const accountDetail = await AccountUser.findOne({
+      _id: id,
+    });
+
+    if (!accountDetail) {
+      res.json({
+        code: "error",
+        message: "Bản ghi không tồn tại!",
+      });
+      return;
+    }
+
+    await AccountUser.deleteOne({
+      _id: id,
+    });
+
+    res.json({
+      code: "success",
+      message: "Đã xóa vĩnh viễn tài khoản!",
+    });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Bản ghi không hợp lệ!",
+    });
+  }
+};
--- routes/admin/account-user.route.ts
+++ routes/admin/account-user.route.ts
@@ -5,4 +5,12 @@ const router = Router();
 
 router.get("/list", accountUserController.list);
 
+router.patch("/delete/:id", accountUserController.deletePatch);
+
+router.get("/trash", accountUserController.trash);
+
+router.patch("/undo/:id", accountUserController.undoPatch);
+
+router.delete("/destroy/:id", accountUserController.destroyDelete);
+
 export default router;
